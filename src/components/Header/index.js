6bf34a6412f8a4b71e014954f56afa6d124fcecd
Marcelo Gaia <marcelozakk07@gmail.com>
2018-06-10 16:43:01 -0700

    Fixed the responsiveness of the Header (navbar)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,26 +9,29 @@ class Header extends Component {
 	render() {
 		return ( 
 			<div className="Header">
-				<Navbar className="Header-navBar" fixedTop>
+				<Navbar className="Header-navbar" fixedTop>
 					<Navbar.Header>
 						<Navbar.Brand>
-							<Link to="#home"><img src={logo} alt="Marcelo Gaia" /></Link>
+							<Link to="#home"><img src={logo} className="logo" alt="Marcelo Gaia" /></Link>
 						</Navbar.Brand>
+						<Navbar.Toggle className="Header-toggle" />
 					</Navbar.Header>
-					<Nav pullRight className="Header-nav">
-						<NavItem eventKey={1} href="#">
-							Blog
-						</NavItem>
-						<NavItem eventKey={2} href="#">
-							About
-						</NavItem>
-						<NavItem eventKey={3} href="#">
-							Projects
-						</NavItem>
-						<NavItem eventKey={4} href="#">
-							Contact
-						</NavItem>
-					</Nav>
+					<Navbar.Collapse>
+						<Nav pullRight className="Header-nav">
+							<NavItem eventKey={1} href="#">
+								Blog
+							</NavItem>
+							<NavItem eventKey={2} href="#">
+								About
+							</NavItem>
+							<NavItem eventKey={3} href="#">
+								Projects
+							</NavItem>
+							<NavItem eventKey={4} href="#">
+								Contact
+							</NavItem>
+						</Nav>
+					</Navbar.Collapse>
 				</Navbar>
 			</div>
 		);
